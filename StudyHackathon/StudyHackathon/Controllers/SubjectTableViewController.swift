51ca61cb81103b9a4368982e1fecad70452abcf9
Selena Sui <selena_sui@yahoo.com>
2018-07-20 11:33:03 -0700

    Changed layout

--- StudyHackathon/StudyHackathon/Controllers/SubjectTableViewController.swift
+++ StudyHackathon/StudyHackathon/Controllers/SubjectTableViewController.swift
@@ -9,23 +9,33 @@
 import UIKit
 import FirebaseDatabase
 
-class SubjectTableViewController: UITableViewController {
+class SubjectTableViewController: UIViewController {
     
-    var subjects = [Subject]() {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+    @IBOutlet weak var tableView: UITableView!
+    
+    var subjects = [Subject]()
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationItem.hidesBackButton = true
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
         
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        addSlideMenuButton()
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        configureTableView()
+        reloadSubjects()
+    }
+    
+    @objc func reloadSubjects() {
         SubjectService.show { (subjects) in
             guard let subjectList = subjects else { return }
             
@@ -37,11 +47,22 @@ class SubjectTableViewController: UITableViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func configureTableView() {
+        tableView.separatorStyle = .none
+    }
+    
+    @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
+        reloadSubjects()
+    }
+}
+
+extension SubjectTableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return subjects.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "subjectTableViewCell", for: indexPath) as! SubjectTableViewCell
         
         let subject = subjects[indexPath.row]
@@ -67,111 +88,8 @@ class SubjectTableViewController: UITableViewController {
     }
 }
 
-extension SubjectTableViewController {
-    func slideMenuItemSelectedAtIndex(_ index: Int32) {
-        let topViewController : UIViewController = self.navigationController!.topViewController!
-        print("View Controller is : \(topViewController) \n", terminator: "")
-        switch(index){
-        case 0:
-            print("Home\n", terminator: "")
-            
-            self.openViewControllerBasedOnIdentifier("Home")
-            
-            break
-        case 1:
-            print("Play\n", terminator: "")
-            
-            self.openViewControllerBasedOnIdentifier("PlayVC")
-            
-            break
-        default:
-            print("default\n", terminator: "")
-        }
-    }
-    
-    func openViewControllerBasedOnIdentifier(_ strIdentifier:String){
-        let destViewController : UIViewController = self.storyboard!.instantiateViewController(withIdentifier: strIdentifier)
-        
-        let topViewController : UIViewController = self.navigationController!.topViewController!
-        
-        if (topViewController.restorationIdentifier! == destViewController.restorationIdentifier!){
-            print("Same VC")
-        } else {
-            self.navigationController!.pushViewController(destViewController, animated: true)
-        }
-    }
-    
-    func addSlideMenuButton(){
-        let btnShowMenu = UIButton(type: UIButtonType.system)
-        btnShowMenu.setImage(self.defaultMenuImage(), for: UIControlState())
-        btnShowMenu.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-        btnShowMenu.addTarget(self, action: #selector(BaseViewController.onSlideMenuButtonPressed(_:)), for: UIControlEvents.touchUpInside)
-        let customBarItem = UIBarButtonItem(customView: btnShowMenu)
-        self.navigationItem.leftBarButtonItem = customBarItem;
-    }
-    
-    func defaultMenuImage() -> UIImage {
-        var defaultMenuImage = UIImage()
-        
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: 30, height: 22), false, 0.0)
-        
-        UIColor.black.setFill()
-        UIBezierPath(rect: CGRect(x: 0, y: 3, width: 30, height: 1)).fill()
-        UIBezierPath(rect: CGRect(x: 0, y: 10, width: 30, height: 1)).fill()
-        UIBezierPath(rect: CGRect(x: 0, y: 17, width: 30, height: 1)).fill()
-        
-        UIColor.white.setFill()
-        UIBezierPath(rect: CGRect(x: 0, y: 4, width: 30, height: 1)).fill()
-        UIBezierPath(rect: CGRect(x: 0, y: 11,  width: 30, height: 1)).fill()
-        UIBezierPath(rect: CGRect(x: 0, y: 18, width: 30, height: 1)).fill()
-        
-        defaultMenuImage = UIGraphicsGetImageFromCurrentImageContext()!
-        
-        UIGraphicsEndImageContext()
-        
-        return defaultMenuImage;
-    }
-    
-    @objc func onSlideMenuButtonPressed(_ sender : UIButton){
-        if (sender.tag == 10)
-        {
-            // To Hide Menu If it already there
-            self.slideMenuItemSelectedAtIndex(-1);
-            
-            sender.tag = 0;
-            
-            let viewMenuBack : UIView = view.subviews.last!
-            
-            UIView.animate(withDuration: 0.3, animations: { () -> Void in
-                var frameMenu : CGRect = viewMenuBack.frame
-                frameMenu.origin.x = -1 * UIScreen.main.bounds.size.width
-                viewMenuBack.frame = frameMenu
-                viewMenuBack.layoutIfNeeded()
-                viewMenuBack.backgroundColor = UIColor.clear
-            }, completion: { (finished) -> Void in
-                viewMenuBack.removeFromSuperview()
-            })
-            
-            return
-        }
-        
-        sender.isEnabled = false
-        sender.tag = 10
-        
-        let menuVC : MenuViewController = self.storyboard!.instantiateViewController(withIdentifier: "MenuViewController") as! MenuViewController
-        menuVC.btnMenu = sender
-        menuVC.delegate = self as? SlideMenuDelegate
-        self.view.addSubview(menuVC.view)
-        self.addChildViewController(menuVC)
-        menuVC.view.layoutIfNeeded()
-        
-        
-        menuVC.view.frame=CGRect(x: 0 - UIScreen.main.bounds.size.width, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height);
-        
-        UIView.animate(withDuration: 0.3, animations: { () -> Void in
-            menuVC.view.frame=CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height);
-            sender.isEnabled = true
-        }, completion:nil)
+extension SubjectTableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return SubjectTableViewCell.height
     }
 }
-
